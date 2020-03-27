9ecafdeb7af28cbf2c805c794140ce30bbc76b2f
ozoloko <contato.matheusluiz@gmail.com>
2020-03-26 22:11:21 -0300

    Finalização do módulo

--- curso-app/src/app/pages/recipes/recipe-list/recipe-list.component.ts
+++ curso-app/src/app/pages/recipes/recipe-list/recipe-list.component.ts
@@ -12,6 +12,16 @@ export class RecipeListComponent implements OnInit {
       "Test Recipe",
       "A Delicious Test",
       "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2019/8/6/0/WU2301_Four-Cheese-Pepperoni-Pizzadilla_s4x3.jpg.rend.hgtvcom.826.620.suffix/1565115622965.jpeg"
+    ),
+    new RecipeModel(
+      "Meatballs",
+      "Spagetti with tomato sauce and meatballs",
+      "https://www.cookingclassy.com/wp-content/uploads/2019/09/meatballs-21.jpg"
+    ),
+    new RecipeModel(
+      "Turkey Mearloaf",
+      "An easy and tasty turky meatloaf recipe",
+      "https://www.skinnytaste.com/wp-content/uploads/2009/02/turkey-meatloaf-8.jpg"
     )
   ];
 
--- curso-app/src/app/pages/shopping/shopping-list/shopping-list.component.ts
+++ curso-app/src/app/pages/shopping/shopping-list/shopping-list.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import { IngredientModel } from "./../../../core/models/ingredient.model";
 
 @Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  selector: "app-shopping-list",
+  templateUrl: "./shopping-list.component.html",
+  styleUrls: ["./shopping-list.component.css"]
 })
 export class ShoppingListComponent implements OnInit {
+  public ingredients: IngredientModel[] = [
+    new IngredientModel("Apples", 5),
+    new IngredientModel("Tomatos", 5)
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
